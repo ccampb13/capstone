5fc61d1de4e5d6c03ec99c6abfbc200474436452
Chad Campbell <ccampb13@gmail.com>
2014-06-19 23:54:51 -0500

    midnight

--- app/static/js/es6/main.es6.js
+++ app/static/js/es6/main.es6.js
@@ -1,18 +1,35 @@
 /* jshint unused:false */
+/* global google:true */
 
 (function(){
   'use strict';
 
   $(document).ready(init);
 
+  let map;
+
 
   function init(){
+    initMap(36.1667, -86.7833, 11);
+    // $('#add').click(searchMap);
     $('#messages').click(getMessages);
     $('#dashboard').click(getDashboard);
     $('#search').click(getSearch);
     $('#tasks').click(getTasks);
     getDashboard();
   }
+
+  //  function add(){
+  //   let zip = $('#zip').val().trim();
+  //   searchMap(zip);
+  // }
+
+  function initMap(lat, lng, zoom){
+    let styles = [{'stylers':[{'hue':'#dd0d0d'}]},{'featureType':'road','elementType':'labels','stylers':[{'visibility':'off'}]},{'featureType':'road','elementType':'geometry','stylers':[{'lightness':100},{'visibility':'simplified'}]}];
+    let mapOptions = {center: new google.maps.LatLng(lat, lng), zoom: zoom, mapTypeId: google.maps.MapTypeId.ROADMAP, styles: styles};  //{is used when adding multiple statements}
+    map = new google.maps.Map(document.getElementById('map'), mapOptions);
+  }
+
 
   function getMessages(e){
     // var age = $('#age').serialize();
